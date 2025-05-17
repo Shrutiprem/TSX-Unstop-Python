690107c6b6bacc8c8ca3c95cf684f4eb80a46eea
Shrutiprem <shrutisreekp@gmail.com>
2025-05-18 00:16:54 +0530

    Added Python interpreter session code

---
+++ interpreter_session.py
@@ -0,0 +1,10 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello, World!")
+Hello, World!
+>>> 5 + 3
+8
+>>> x = 10
+>>> x * 2
+20
+>>> exit()
